e9a9b2c07755dd0852f313fed4b428ff83e3ceaf
githusr <emailusers@qq.com>
2024-07-19 21:14:00 +0800

    first commit

---
+++ include/loginwidget.h
@@ -0,0 +1,23 @@
+#ifndef LOGINWIDGET_H
+#define LOGINWIDGET_H
+
+#include <QWidget>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class LoginWidget;
+}
+QT_END_NAMESPACE
+
+class LoginWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    LoginWidget(QWidget *parent = nullptr);
+    ~LoginWidget();
+
+private:
+    Ui::LoginWidget *ui;
+};
+#endif // LOGINWIDGET_H
---
+++ src/loginwidget.cpp
@@ -0,0 +1,14 @@
+#include "../include/loginwidget.h"
+#include "forms/ui_loginwidget.h"
+
+LoginWidget::LoginWidget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::LoginWidget)
+{
+    ui->setupUi(this);
+}
+
+LoginWidget::~LoginWidget()
+{
+    delete ui;
+}
